d9c4b9de5fb1ca6716172dd8ffe52ebb0049fd09
Théophile Choutri <theophile@choutri.eu>
2021-03-22 13:56:35 +0100

    Remove debug info

--- src/Web/Controller/FlashMessage.hs
+++ src/Web/Controller/FlashMessage.hs
@@ -2,12 +2,10 @@ module Web.Controller.FlashMessage where
 
 import Web.Scotty.Trans (ActionT)
 import Web.Session (getAssign, insertAssign, modifySession, readSession, removeAssign)
-import Web.Types (WebEnvironment (sessions), WebM, printAssigns)
+import Web.Types (WebEnvironment (sessions), WebM)
 
 putInfo :: Text -> ActionT LText WebM ()
-putInfo msg = do
-  putTextLn $ "=== Inserting info: " <> msg
-  putFlash "alert_info" msg
+putInfo msg = putFlash "alert_info" msg
 
 putWarning :: Text -> ActionT LText WebM ()
 putWarning msg = putFlash "alert_warning" msg
@@ -16,9 +14,7 @@ putError :: Text -> ActionT LText WebM ()
 putError msg = putFlash "alert_error" msg
 
 getInfo :: ActionT LText WebM (Maybe Text)
-getInfo = do
-  putTextLn "=== Getting info flash"
-  popFlash "alert_info"
+getInfo = popFlash "alert_info"
 
 getWarning :: ActionT LText WebM (Maybe Text)
 getWarning = popFlash "alert_warning"
@@ -29,11 +25,7 @@ getError = popFlash "alert_error"
 putFlash :: Text -> Text -> ActionT LText WebM ()
 putFlash key value = do
   sm <- asks sessions
-  putTextLn "=== Before putting the flash"
-  printAssigns
   modifySession sm (\mVal -> mVal >>= Just . insertAssign key value)
-  putTextLn "=== After putting the flash"
-  printAssigns
 
 popFlash :: Text -> ActionT LText WebM (Maybe Text)
 popFlash flash = do
@@ -42,9 +34,6 @@ popFlash flash = do
   case mUserAssigns of
     Nothing -> pure Nothing
     Just ua -> do
-      traceM "=== user assigns when poping the flash"
-      traceShowM ua
       let content = getAssign flash ua
       modifySession sm (\mVal -> mVal >>= Just . removeAssign flash)
-      liftIO $ putTextLn $ "=== Flash content: " <> fromMaybe "<nothing>" content
       pure content
